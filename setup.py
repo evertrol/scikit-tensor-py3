2ca38788dcb4c9717279ba9a79d6c485c6f5618b
Evert Rol <evert.rol@gmail.com>
2018-08-21 15:06:54 +0200

    Require Python 3.4 at minimum

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ def setup_package():
         download_url=DOWNLOAD_URL,
         long_description=LONG_DESCRIPTION,
         version=get_version(),
+        python_requires=">=3.4",
         install_requires=[
             'numpy',
             'scipy',
